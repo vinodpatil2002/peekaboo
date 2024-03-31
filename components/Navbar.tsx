b5d34d972267032bcd6cc966edc553cd4b9d8f3b
vinodpatil2002 <vinodnspatil@gmail.com>
2024-04-01 04:24:35 +0530

    Add Navbar with responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex justify-between items-center fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+      <Link href="/" className='flex items-center gap-1'>
+        <Image src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="Logo"
+          className='max-sm:size-10'
+        />
+        <p className='text-[26px] font-bold text-white max-sm:hidden'>
+          PeekaBoo
+        </p>
+      </Link>
+      <div className="flex justify-between items-center gap-5 ">
+        {/* clerk user management */}
+        <MobileNav />
+      </div>
+    </nav>
   )
 }
 
