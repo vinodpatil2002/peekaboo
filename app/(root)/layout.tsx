0522417b98625fddd09f3cd624bd1c4e9cd318ab
vinodpatil2002 <vinodnspatil@gmail.com>
2024-04-02 18:31:48 +0530

    Complete New Meeting functionality

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,16 @@
 import { ReactNode } from 'react';
 
 import StreamVideoProvider from '@/providers/StreamClientProvider';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "PEEKABOO",
+  description: "Video calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
 
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
