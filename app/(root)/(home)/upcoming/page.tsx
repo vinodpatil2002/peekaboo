d36875affe6214a5b992734b533287cc9a956d13
vinodpatil2002 <vinodnspatil@gmail.com>
2024-04-03 03:32:28 +0530

    Complete Upcoming meetings and Previous meetings functionalities

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,10 +1,15 @@
+import CallList from '@/components/CallList'
 import React from 'react'
 
 const Upcoming = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
-      <h1 className='text-4xl font-bold'>Upcoming</h1>
-      
+      <h1 className='text-4xl font-bold'>
+        Upcoming
+      </h1>
+      <CallList 
+        type='upcoming'
+      />
     </section>
   )
 }
