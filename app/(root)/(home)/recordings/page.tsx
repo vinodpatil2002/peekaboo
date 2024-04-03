40d88999b26a9ed54541062f782aac45ef3f3705
vinodpatil2002 <vinodnspatil@gmail.com>
2024-04-03 17:36:40 +0530

    Complete  Get Recordings functionality

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,10 +1,11 @@
+import CallList from '@/components/CallList'
 import React from 'react'
 
 const Recordings = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
       <h1 className='text-4xl font-bold'>Recordings</h1>
-      
+      <CallList type='recordings' />
     </section>
   )
 }
